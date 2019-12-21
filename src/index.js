36b015716455016b9e8485a616fa634bb132adb3
shindongri <dongri@terafunding.com>
2019-12-21 16:15:50 +0900

    Add SW

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+
 const App = () => <div className="app">Hello Azure World</div>
 
 ReactDOM.render(<App />, document.getElementById('app'))
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => { // 로딩 완료되고 캐싱 되게
+        navigator.serviceWorker.register('/service-worker.js')
+    })
+}
